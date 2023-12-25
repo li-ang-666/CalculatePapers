7fda6ef191c8ebf97d2694d99481c21619784af0
liang <li_ang_666@163.com>
2023-12-25 10:05:36 +0800

    1

--- page3.sql
+++ page3.sql
@@ -5,30 +5,6 @@ t1 as(
     boss_human_pid,
     company_gid,
     partner_human_pid,
-    named_struct(
-      'boss_human_gid', boss_human_gid,
-      'boss_human_pid', boss_human_pid,
-      'boss_human_name', boss_human_name,
-      'boss_identity', boss_identity,
-      'boss_shares', boss_shares,
-      'company_gid', company_gid,
-      'company_name', company_name,
-      'company_registered_status', company_registered_status,
-      'company_registered_capital', company_registered_capital,
-      's', s,
-      'partner_human_gid', partner_human_gid,
-      'partner_human_pid', partner_human_pid,
-      'partner_human_name', partner_human_name,
-      'partner_identity', partner_identity,
-      'partner_shares', partner_shares,
-      'single_cooperation_score', single_cooperation_score,
-      'multi_cooperation_score', multi_cooperation_score,
-      'single_cooperation_row_number', single_cooperation_row_number,
-      'multi_cooperation_dense_rank', multi_cooperation_dense_rank,
-      'cooperation_times_with_this_partner', cooperation_times_with_this_partner,
-      'cooperation_times_with_all_partner', cooperation_times_with_all_partner,
-      'total_partners', total_partners
-    ) column_map,
     sha2(concat(
       nvl(boss_human_gid, 'NULL'),
       nvl(boss_human_pid, 'NULL'),
@@ -61,30 +37,6 @@ t2 as(
     boss_human_pid,
     company_gid,
     partner_human_pid,
-    named_struct(
-      'boss_human_gid', boss_human_gid,
-      'boss_human_pid', boss_human_pid,
-      'boss_human_name', boss_human_name,
-      'boss_identity', boss_identity,
-      'boss_shares', boss_shares,
-      'company_gid', company_gid,
-      'company_name', company_name,
-      'company_registered_status', company_registered_status,
-      'company_registered_capital', company_registered_capital,
-      's', s,
-      'partner_human_gid', partner_human_gid,
-      'partner_human_pid', partner_human_pid,
-      'partner_human_name', partner_human_name,
-      'partner_identity', partner_identity,
-      'partner_shares', partner_shares,
-      'single_cooperation_score', single_cooperation_score,
-      'multi_cooperation_score', multi_cooperation_score,
-      'single_cooperation_row_number', single_cooperation_row_number,
-      'multi_cooperation_dense_rank', multi_cooperation_dense_rank,
-      'cooperation_times_with_this_partner', cooperation_times_with_this_partner,
-      'cooperation_times_with_all_partner', cooperation_times_with_all_partner,
-      'total_partners', total_partners
-    ) column_map,
     sha2(concat(
       nvl(boss_human_gid, 'NULL'),
       nvl(boss_human_pid, 'NULL'),
@@ -108,7 +60,31 @@ t2 as(
       nvl(cooperation_times_with_this_partner, 'NULL'),
       nvl(cooperation_times_with_all_partner, 'NULL'),
       nvl(total_partners, 'NULL')
-    ), 512) cipher
+    ), 512) cipher,
+    to_json(named_struct(
+      'boss_human_gid', boss_human_gid,
+      'boss_human_pid', boss_human_pid,
+      'boss_human_name', boss_human_name,
+      'boss_identity', boss_identity,
+      'boss_shares', boss_shares,
+      'company_gid', company_gid,
+      'company_name', company_name,
+      'company_registered_status', company_registered_status,
+      'company_registered_capital', company_registered_capital,
+      's', s,
+      'partner_human_gid', partner_human_gid,
+      'partner_human_pid', partner_human_pid,
+      'partner_human_name', partner_human_name,
+      'partner_identity', partner_identity,
+      'partner_shares', partner_shares,
+      'single_cooperation_score', single_cooperation_score,
+      'multi_cooperation_score', multi_cooperation_score,
+      'single_cooperation_row_number', single_cooperation_row_number,
+      'multi_cooperation_dense_rank', multi_cooperation_dense_rank,
+      'cooperation_times_with_this_partner', cooperation_times_with_this_partner,
+      'cooperation_times_with_all_partner', cooperation_times_with_all_partner,
+      'total_partners', total_partners
+    )) column_map
   from hudi_ads.cooperation_partner_new where pt = $pt
 )
 insert overwrite table hudi_ads.cooperation_partner_diff partition(pt = $pt)
@@ -118,7 +94,7 @@ select
   nvl(t2.partner_human_pid, t1.partner_human_pid) partner_human_pid,
   nvl(t2.company_gid, t1.company_gid) company_gid,
   -- values 恒取最新
-  to_json(t2.column_map) column_map
+  t2.column_map column_map
 from t1
 full outer join t2
   on t1.boss_human_pid = t2.boss_human_pid
