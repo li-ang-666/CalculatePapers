f37c1138a943d3add536d3b7e1cc1c48f204658e
liang <li_ang_666@163.com>
2023-12-25 17:58:05 +0800

    1

--- page1.sql
+++ page1.sql
@@ -1,40 +1,18 @@
-drop table if exists hudi_ads.cooperation_partner;
-create table if not exists hudi_ads.cooperation_partner(
-  -- 老板
-  boss_human_gid decimal(20, 0) comment '老板gid',
-  boss_human_pid string comment '老板pid',
-  boss_human_name string comment '老板名称',
-  boss_identity string comment '老板身份',
-  boss_shares decimal(32, 12) comment '老板持股(分)',
-  -- 公司
-  company_gid decimal(20, 0) comment '公司gid',
-  company_name string comment '公司名称',
-  company_registered_status string comment '公司经营状态',
-  company_registered_capital decimal(20, 0) comment '公司注册资本(分)',
-  s int comment '计算公式中的S, 公司为注吊销, 则为0, 否则为1',
-  -- 伙伴
-  partner_human_gid decimal(20, 0) comment '合作伙伴gid',
-  partner_human_pid string comment '合作伙伴pid',
-  partner_human_name string comment '合作伙伴名称',
-  partner_identity string comment '合作伙伴身份',
-  partner_shares decimal(32, 12) comment '合作伙伴持股(分)',
-  -- 合作深度
-  single_cooperation_score decimal(32, 12) comment '单次合作深度',
-  multi_cooperation_score decimal(32, 12) comment '综合合作深度',
-  single_cooperation_row_number int comment '单次合作深度排序',
-  multi_cooperation_dense_rank int comment '综合合作深度排序',
-  -- 补加
-  cooperation_times_with_this_partner int comment '老板与该伙伴合作次数',
-  cooperation_times_with_all_partner int comment '老板与所有伙伴合作次数',
-  total_partners int comment '老板所有合作伙伴数'
-)partitioned by (pt int comment 'yyyyMMdd');
-
-drop table if exists hudi_ads.cooperation_partner_diff;
-create table if not exists hudi_ads.cooperation_partner_diff(
-  -- unique key
-  boss_human_pid string comment '老板pid',
-  partner_human_pid string comment '合作伙伴pid',
-  company_gid decimal(20, 0) comment '公司gid',
-  -- 其他
-  column_map string comment 'columnMap'
-)partitioned by (pt int comment 'yyyyMMdd');
+use operating_info;
+drop table if exists `company_bid_parsed_info_patch`;
+CREATE TABLE if not exists `company_bid_parsed_info_patch` like `operating_info`.`company_bid_parsed_info`;
+alter table  company_bid_parsed_info_patch
+add column `party_a`               longtext comment '招标方',
+add column `agent`                 longtext comment '代理方',
+add column `tenderer`              longtext comment '投标方',
+add column `candidate`             longtext comment '候选方',
+add column `winner`                longtext comment '中标方',
+add column `winner_amt`            longtext comment '中标金额',
+add column `mention`               longtext comment '其他提及',
+add column `item_no`               varchar(255) not null default '' comment '项目编号',
+add column `bid_deadline`          datetime comment '投标截止时间',
+add column `bid_download_deadline` datetime comment '标书获取截止时间',
+add column `province`              varchar(50) not null default '' comment '省',
+add column `city`                  varchar(50) not null default '' comment '市',
+add column `publish_year`          int comment '发布年份',
+add column `roles`                 longtext comment '所有涉及角色';
