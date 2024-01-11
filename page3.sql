5685d9e8ba721765b9ab008b8d14be3fa1e8ad18
liang <li_ang_666@163.com>
2024-01-11 22:24:52 +0800

    1

---
+++ page3.sql
@@ -0,0 +1,27 @@
+use test_db;
+-- SET show_hidden_columns=true;
+-------------------------------------------------------
+drop table if exists unique_test;
+create table if not exists unique_test(id int not null, name text)
+UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
+PROPERTIES (
+"replication_num" = "1",
+"in_memory" = "false"
+);
+-------------------------------------------------------
+drop table if exists agg_test;
+create table if not exists agg_test(id int not null, name text REPLACE_IF_NOT_NULL)
+AGGREGATE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 1
+PROPERTIES (
+"replication_num" = "1",
+"in_memory" = "false"
+);
+
+
+
+
+select * from agg_test;
+SET enable_insert_strict = true;
+insert into agg_test(id,name)values('aaa','bbb');
+select * from agg_test;
+
