7a361dcd52a52cdfb6633898e39fdc410769632b
liang <li_ang_666@163.com>
2024-01-12 14:58:24 +0800

    1

--- page3.sql
+++ page3.sql
@@ -1,15 +1,20 @@
 use test_db;
--- SET show_hidden_columns=true;
--------------------------------------------------------
 drop table if exists unique_test force;
 create table if not exists unique_test(
   id LARGEINT NOT NULL,
+  pt INT NOT NULL,
   name STRING
-)UNIQUE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 6;
--------------------------------------------------------
--- drop table if exists agg_test force;
--- create table if not exists agg_test(
---   id LARGEINT NOT NULL,
---   name STRING REPLACE_IF_NOT_NULL
--- )AGGREGATE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 6;
-;
+)
+UNIQUE KEY (`id`, `pt`)
+PARTITION BY RANGE (`pt`) ()
+DISTRIBUTED BY HASH (`id`) BUCKETS 6
+PROPERTIES(
+  "replication_allocation" = "tag.location.default: 3",
+  "dynamic_partition.enable" = "true",
+  "dynamic_partition.time_unit" = "DAY",
+  "dynamic_partition.start" = "-1",
+  "dynamic_partition.end" = "1",
+  "dynamic_partition.prefix" = "p",
+  "dynamic_partition.buckets" = "6",
+  "dynamic_partition.replication_allocation" = "tag.location.default: 3"
+);
