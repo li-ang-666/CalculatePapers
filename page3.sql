831f59dcefcc5860480517106b38fc18119adb97
liang <li_ang_666@163.com>
2024-01-12 10:23:08 +0800

    1

--- page3.sql
+++ page3.sql
@@ -12,12 +12,3 @@ create table if not exists agg_test(
   id int not null,
   name text REPLACE_IF_NOT_NULL
 )AGGREGATE KEY(`id`) DISTRIBUTED BY HASH(`id`) BUCKETS 36;
-
-
-
-
-select * from agg_test;
-SET enable_insert_strict = true;
-insert into agg_test(id,name)values('aaa','bbb');
-select * from agg_test;
-
