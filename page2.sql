b618a55b125f7ae54ab9b2a0bd40ef9c414552b6
liang <li_ang_666@163.com>
2024-01-12 16:03:38 +0800

    1

--- page2.sql
+++ page2.sql
@@ -8,14 +8,5 @@ admin set frontend config("disable_balance" = "false");
 admin set frontend config("disable_colocate_balance" = "false");
 admin set frontend config("disable_tablet_scheduler" = "false");
 
-./spark-submit.sh 
 
 doris --sql "insert into test_db.unique_test select date_format(date_add(current_date(),-1),'yyyyMMdd') pt, id, company_name name from hudi_ods.company_index limit 1000000" --parallelism 32
-
-
-./test.sh --sql "insert into test_db.unique_test select date_format(date_add(current_date(),-1),'yyyyMMdd') pt, id, company_name name from hudi_ods.company_index" --parallelism 32
-
-sql="${1}${2}${3}${4}"
-insert_sql=$(echo "$sql" | grep -oE 'insert\s+into\s+\w+\.\w+')
-res=$(echo "$insert_sql" | grep -oE '\w+\.\w+')
-
