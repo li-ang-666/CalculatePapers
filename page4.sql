2067e590a140a77c7b12f62caa8aa2caa8fcb2a1
liang <li_ang_666@163.com>
2024-01-13 22:56:35 +0800

    1

--- page4.sql
+++ page4.sql
@@ -2,12 +2,12 @@ DROP TABLE `test_db`.`dwd_user_register_details`;
 CREATE TABLE `test_db`.`dwd_user_register_details` (
   `tyc_user_id` largeint COMMENT '天眼查用户ID',
   `mobile` largeint COMMENT '手机号',
-  `register_time` datetime COMMENT '注册时间',
-  `vip_from_time` datetime COMMENT 'VIP开始时间',
-  `vip_to_time` datetime COMMENT 'VIP结束日期',
+  `register_time` datetimev2(6) COMMENT '注册时间',
+  `vip_from_time` datetimev2(6) COMMENT 'VIP开始时间',
+  `vip_to_time` datetimev2(6) COMMENT 'VIP结束日期',
   `user_type` largeint COMMENT '用户类型 0:普通,1:vvip,2:媒体用户,3:3个月vip,4:6个月vip,5:12个月以上vip,6:24个月vip,7:26个月以上vip,-1:删除,-2:黑名单',
-  `create_time` datetime COMMENT '写入doris时间',
-  `update_time` datetime COMMENT '更新doris时间'
+  `create_time` datetimev2(6) COMMENT '写入doris时间',
+  `update_time` datetimev2(6) COMMENT '更新doris时间'
 ) ENGINE=OLAP
 UNIQUE KEY(`tyc_user_id`)
 COMMENT '注册'
@@ -27,11 +27,11 @@ CREATE TABLE `test_db`.`dwd_coupon_info` (
   `unique_user_id` largeint COMMENT '天眼查用户ID',
   `promotion_id` largeint COMMENT '优惠ID',
   `use_status` largeint COMMENT '使用状态',
-  `receive_time` datetime COMMENT '发券时间',
-  `effective_time` datetime COMMENT '有效开始日期',
-  `expiration_time` datetime COMMENT '有效结束日期',
-  `create_time` datetime COMMENT '写入doris时间',
-  `update_time` datetime COMMENT '更新doris时间'
+  `receive_time` datetimev2(6) COMMENT '发券时间',
+  `effective_time` datetimev2(6) COMMENT '有效开始日期',
+  `expiration_time` datetimev2(6) COMMENT '有效结束日期',
+  `create_time` datetimev2(6) COMMENT '写入doris时间',
+  `update_time` datetimev2(6) COMMENT '更新doris时间'
 ) ENGINE=OLAP
 UNIQUE KEY(`promotion_code`)
 COMMENT '优惠券'
@@ -48,7 +48,7 @@ PROPERTIES (
 DROP TABLE `test_db`.`dwd_app_active`;
 CREATE TABLE `test_db`.`dwd_app_active` (
   `app_id2` varchar(65530) COMMENT 'app_id2唯一键',
-  `pt` date COMMENT '分区',
+  `pt` datev2 COMMENT '分区',
   `android_id` string COMMENT '安卓用户设备标识,不唯一',
   `imei` string COMMENT '安卓用户设备唯一标识,安卓10及以上,无法获取',
   `oaid` string COMMENT '安卓用户设备标识,无需授权,不唯一,可手动重置',
@@ -56,9 +56,9 @@ CREATE TABLE `test_db`.`dwd_app_active` (
   `idfv` string COMMENT 'iOS用户设备标识,无需授权,不唯一,卸载重装会重置',
   `type` largeint COMMENT '设备类型 iOS:1 安卓:0',
   `umeng_channel` string COMMENT '渠道包,如huawei,iOS1',
-  `create_time` datetime COMMENT '激活时间',
+  `create_time` datetimev2(6) COMMENT '激活时间',
   `app_version` string COMMENT 'app版本',
-  `update_time` datetime COMMENT '更新doris时间'
+  `update_time` datetimev2(6) COMMENT '更新doris时间'
 ) ENGINE=OLAP
 UNIQUE KEY(`app_id2`, `pt`)
 COMMENT '激活'
