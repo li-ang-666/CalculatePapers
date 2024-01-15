f563384eae48a7de225f32d8ee39f83f8ecea3ce
liang <li_ang_666@163.com>
2024-01-15 11:52:30 +0800

    1

--- page2.sql
+++ page2.sql
@@ -8,5 +8,5 @@ admin set frontend config("disable_colocate_balance" = "false");
 admin set frontend config("disable_tablet_scheduler" = "false");
 
 doris \
-  --parallelism 32 \
-  --sql "insert into test_db.unique_test select date_format(date_add(current_date(),-1),'yyyyMMdd') pt, id, company_name name from hudi_ods.company_index"
+  --parallelism 64 \
+  --sql "insert into test_db.ads_user_dispatch_log_tag_df select date_add(current_date(),-1) pt, tyc_user_id, dispatch_id,dispatch_date,mobile from ads.ads_user_dispatch_log_tag_df where pt = 20240114"
--- page4.sql
+++ page4.sql
@@ -1,5 +1,5 @@
-DROP TABLE `test_db`.`dwd_user_register_details`;
-CREATE TABLE `test_db`.`dwd_user_register_details` (
+DROP TABLE `dwd`.`dwd_user_register_details`;
+CREATE TABLE `dwd`.`dwd_user_register_details` (
   `tyc_user_id` largeint COMMENT '天眼查用户ID',
   `mobile` largeint COMMENT '手机号',
   `register_time` datetimev2(6) COMMENT '注册时间',
@@ -11,14 +11,14 @@ CREATE TABLE `test_db`.`dwd_user_register_details` (
 ) ENGINE=OLAP
 UNIQUE KEY(`tyc_user_id`)
 COMMENT '注册'
-DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 6
+DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 30
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
 );
 -----------------------------------------------------------------------------------------------------
-DROP TABLE `test_db`.`dwd_coupon_info`;
-CREATE TABLE `test_db`.`dwd_coupon_info` (
+DROP TABLE `dwd`.`dwd_coupon_info`;
+CREATE TABLE `dwd`.`dwd_coupon_info` (
   `promotion_code` varchar(255) COMMENT '优惠码',
   `unique_user_id` largeint COMMENT '天眼查用户ID',
   `promotion_id` largeint COMMENT '优惠ID',
@@ -31,14 +31,14 @@ CREATE TABLE `test_db`.`dwd_coupon_info` (
 ) ENGINE=OLAP
 UNIQUE KEY(`promotion_code`)
 COMMENT '优惠券'
-DISTRIBUTED BY HASH(`promotion_code`) BUCKETS 6
+DISTRIBUTED BY HASH(`promotion_code`) BUCKETS 30
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
 );
 -----------------------------------------------------------------------------------------------------
-DROP TABLE `test_db`.`dwd_app_active`;
-CREATE TABLE `test_db`.`dwd_app_active` (
+DROP TABLE `dwd`.`dwd_app_active`;
+CREATE TABLE `dwd`.`dwd_app_active` (
   `app_id2` varchar(255) COMMENT 'app_id2唯一键',
   `pt` datev2 COMMENT '分区',
   `android_id` string COMMENT '安卓用户设备标识,不唯一',
@@ -55,7 +55,7 @@ CREATE TABLE `test_db`.`dwd_app_active` (
 UNIQUE KEY(`app_id2`, `pt`)
 COMMENT '激活'
 PARTITION BY RANGE(`pt`) ()
-DISTRIBUTED BY HASH(`app_id2`) BUCKETS 6
+DISTRIBUTED BY HASH(`app_id2`) BUCKETS 5
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3",
@@ -66,15 +66,11 @@ PROPERTIES (
   "dynamic_partition.history_partition_num" = "30",
   "dynamic_partition.start" = "-30",
   "dynamic_partition.end" = "1",
-  "dynamic_partition.prefix" = "p",
-  -- partition 分桶数
-  "dynamic_partition.buckets" = "6",
-  -- partition 副本数
-  "dynamic_partition.replication_allocation" = "tag.location.default: 3"
+  "dynamic_partition.prefix" = "p"
 );
 -----------------------------------------------------------------------------------------------------
-DROP TABLE `test_db`.`dim_user_comparison`;
-CREATE TABLE `test_db`.`dim_user_comparison` (
+DROP TABLE `dim`.`dim_user_comparison`;
+CREATE TABLE `dim`.`dim_user_comparison` (
   `uid` largeint COMMENT '用户关联',
   `tyc_user_id` largeint COMMENT '老用户ID',
   `tyc_user_id_new` largeint COMMENT '新用户ID',
@@ -84,14 +80,14 @@ CREATE TABLE `test_db`.`dim_user_comparison` (
 ) ENGINE=OLAP
 UNIQUE KEY(`uid`)
 COMMENT '新老用户映射'
-DISTRIBUTED BY HASH(`uid`) BUCKETS 6
+DISTRIBUTED BY HASH(`uid`) BUCKETS 30
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
 );
 -----------------------------------------------------------------------------------------------------
-DROP TABLE `test_db`.`dwd_order_info`;
-CREATE TABLE `test_db`.`dwd_order_info` (
+DROP TABLE `dwd`.`dwd_order_info`;
+CREATE TABLE `dwd`.`dwd_order_info` (
   `order_id` varchar(255) COMMENT '订单ID',
   `order_code` string COMMENT '新交易订单ID',
   `tyc_user_id` largeint COMMENT '天眼查用户ID',
@@ -113,7 +109,7 @@ CREATE TABLE `test_db`.`dwd_order_info` (
 ) ENGINE=OLAP
 UNIQUE KEY(`order_id`)
 COMMENT '订单'
-DISTRIBUTED BY HASH(`order_id`) BUCKETS 6
+DISTRIBUTED BY HASH(`order_id`) BUCKETS 30
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
