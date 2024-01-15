3d70d2f8211b3e6ed5715fa175c51e826e3a34d7
liang <li_ang_666@163.com>
2024-01-15 13:57:18 +0800

    1

--- page2.sql
+++ page2.sql
@@ -1,12 +1,13 @@
-
-admin set frontend config("disable_balance" = "true");
-admin set frontend config("disable_colocate_balance" = "true");
-admin set frontend config("disable_tablet_scheduler" = "true");
-
-admin set frontend config("disable_balance" = "false");
-admin set frontend config("disable_colocate_balance" = "false");
-admin set frontend config("disable_tablet_scheduler" = "false");
-
+sql="insert into test_db.ads_user_dispatch_log_tag_df
+select
+  date_add(current_date(),-1) pt,
+  tyc_user_id,
+  dispatch_id,
+  dispatch_date,
+  mobile
+from ads.ads_user_dispatch_log_tag_df
+where pt = 20240114"
+parallelism="128"
 doris \
-  --parallelism 64 \
-  --sql "insert into test_db.ads_user_dispatch_log_tag_df select date_add(current_date(),-1) pt, tyc_user_id, dispatch_id,dispatch_date,mobile from ads.ads_user_dispatch_log_tag_df where pt = 20240114"
+  -parallelism ${parallelism} \
+  -sql ${sql}
