541fbd37a8285016ad98f72f763700bb3d63fa3a
liang <li_ang_666@163.com>
2024-01-15 17:21:21 +0800

    1

--- page4.sql
+++ page4.sql
@@ -25,6 +25,7 @@ from dwd.dwd_order_info;
 
 
 admin set frontend config("max_running_txn_num_per_db"="10240");
+admin set frontend config("min_replication_num_per_tablet"="3");
 curl -X POST http://10.99.201.177:8040/api/update_config?max_tablet_version_num=10240\&persist=true
 curl -X POST http://10.99.197.74:8040/api/update_config?max_tablet_version_num=10240\&persist=true
 curl -X POST http://10.99.194.79:8040/api/update_config?max_tablet_version_num=10240\&persist=true
