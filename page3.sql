e7624e64ce50188b94e5edce3e535a68ff9ab393
liang <li_ang_666@163.com>
2024-01-15 15:48:14 +0800

    1

--- page3.sql
+++ page3.sql
@@ -1,116 +1,20 @@
-DROP TABLE `dwd`.`dwd_user_register_details`;
-CREATE TABLE `dwd`.`dwd_user_register_details` (
-  `tyc_user_id` largeint COMMENT '天眼查用户ID',
-  `mobile` largeint COMMENT '手机号',
-  `register_time` datetimev2(6) COMMENT '注册时间',
-  `vip_from_time` datetimev2(6) COMMENT 'VIP开始时间',
-  `vip_to_time` datetimev2(6) COMMENT 'VIP结束日期',
-  `user_type` largeint COMMENT '用户类型 0:普通,1:vvip,2:媒体用户,3:3个月vip,4:6个月vip,5:12个月以上vip,6:24个月vip,7:26个月以上vip,-1:删除,-2:黑名单',
-  `create_time` datetimev2(6) COMMENT '写入doris时间',
-  `update_time` datetimev2(6) COMMENT '更新doris时间'
-) ENGINE=OLAP
-UNIQUE KEY(`tyc_user_id`)
-COMMENT '注册'
-DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 30
-PROPERTIES (
-  -- 副本数
-  "replication_allocation" = "tag.location.default: 3"
-);
------------------------------------------------------------------------------------------------------
-DROP TABLE `dwd`.`dwd_coupon_info`;
-CREATE TABLE `dwd`.`dwd_coupon_info` (
-  `promotion_code` varchar(255) COMMENT '优惠码',
-  `unique_user_id` largeint COMMENT '天眼查用户ID',
-  `promotion_id` largeint COMMENT '优惠ID',
-  `use_status` largeint COMMENT '使用状态',
-  `receive_time` datetimev2(6) COMMENT '发券时间',
-  `effective_time` datetimev2(6) COMMENT '有效开始日期',
-  `expiration_time` datetimev2(6) COMMENT '有效结束日期',
-  `create_time` datetimev2(6) COMMENT '写入doris时间',
-  `update_time` datetimev2(6) COMMENT '更新doris时间'
-) ENGINE=OLAP
-UNIQUE KEY(`promotion_code`)
-COMMENT '优惠券'
-DISTRIBUTED BY HASH(`promotion_code`) BUCKETS 30
-PROPERTIES (
-  -- 副本数
-  "replication_allocation" = "tag.location.default: 3"
-);
------------------------------------------------------------------------------------------------------
-DROP TABLE `dwd`.`dwd_app_active`;
-CREATE TABLE `dwd`.`dwd_app_active` (
-  `app_id2` varchar(255) COMMENT 'app_id2唯一键',
-  `pt` datev2 COMMENT '分区',
-  `android_id` string COMMENT '安卓用户设备标识,不唯一',
-  `imei` string COMMENT '安卓用户设备唯一标识,安卓10及以上,无法获取',
-  `oaid` string COMMENT '安卓用户设备标识,无需授权,不唯一,可手动重置',
-  `idfa` string COMMENT 'iOS用户设备标识,需授权',
-  `idfv` string COMMENT 'iOS用户设备标识,无需授权,不唯一,卸载重装会重置',
-  `type` largeint COMMENT '设备类型 iOS:1 安卓:0',
-  `umeng_channel` string COMMENT '渠道包,如huawei,iOS1',
-  `create_time` datetimev2(6) COMMENT '激活时间',
-  `app_version` string COMMENT 'app版本',
-  `update_time` datetimev2(6) COMMENT '更新doris时间'
-) ENGINE=OLAP
-UNIQUE KEY(`app_id2`, `pt`)
-COMMENT '激活'
-PARTITION BY RANGE(`pt`) ()
-DISTRIBUTED BY HASH(`app_id2`) BUCKETS 5
-PROPERTIES (
-  -- 副本数
-  "replication_allocation" = "tag.location.default: 3",
-  -- partition
-  "dynamic_partition.enable" = "true",
-  "dynamic_partition.time_unit" = "DAY",
-  "dynamic_partition.create_history_partition" = "true",
-  "dynamic_partition.history_partition_num" = "30",
-  "dynamic_partition.start" = "-30",
-  "dynamic_partition.end" = "1",
-  "dynamic_partition.prefix" = "p"
-);
------------------------------------------------------------------------------------------------------
-DROP TABLE `dim`.`dim_user_comparison`;
-CREATE TABLE `dim`.`dim_user_comparison` (
-  `uid` largeint COMMENT '用户关联',
-  `tyc_user_id` largeint COMMENT '老用户ID',
-  `tyc_user_id_new` largeint COMMENT '新用户ID',
-  `mobile` largeint COMMENT '手机号',
-  `create_time` datetimev2(6) COMMENT '写入doris时间',
-  `update_time` datetimev2(6) COMMENT '更新doris时间'
-) ENGINE=OLAP
-UNIQUE KEY(`uid`)
-COMMENT '新老用户映射'
-DISTRIBUTED BY HASH(`uid`) BUCKETS 30
-PROPERTIES (
-  -- 副本数
-  "replication_allocation" = "tag.location.default: 3"
-);
------------------------------------------------------------------------------------------------------
-DROP TABLE `dwd`.`dwd_order_info`;
-CREATE TABLE `dwd`.`dwd_order_info` (
-  `order_id` varchar(255) COMMENT '订单ID',
-  `order_code` string COMMENT '新交易订单ID',
-  `tyc_user_id` largeint COMMENT '天眼查用户ID',
-  `mobile` largeint COMMENT '手机号',
-  `sku_id` largeint COMMENT '商品ID',
-  `vip_from_time` datetimev2(6) COMMENT 'VIP开始时间',
-  `vip_to_time` datetimev2(6) COMMENT 'VIP结束日期',
-  `order_status` largeint COMMENT '订单状态',
-  `amount` largeint COMMENT '总金额',
-  `actual_amount` largeint COMMENT '实收金额',
-  `invite_code` string COMMENT '邀请码',
-  `pay_date` datetimev2(6) COMMENT '支付时间',
-  `create_date` datetimev2(6) COMMENT '下单时间',
-  `platform_name` string COMMENT '平台名称 ',
-  `pay_way` string COMMENT '支付方式',
-  `pay_point_id` string COMMENT '痒点ID',
-  `create_time` datetimev2(6) COMMENT '写入doris时间',
-  `update_time` datetimev2(6) COMMENT '更新doris时间'
-) ENGINE=OLAP
-UNIQUE KEY(`order_id`)
-COMMENT '订单'
-DISTRIBUTED BY HASH(`order_id`) BUCKETS 30
-PROPERTIES (
-  -- 副本数
-  "replication_allocation" = "tag.location.default: 3"
-);
+sparkSql="
+select
+  date_add(current_date(),-1) pt,
+  tyc_user_id,
+  dispatch_id,
+  dispatch_date,
+  mobile
+from
+  ads.ads_user_dispatch_log_tag_df
+where
+  pt = 20240114
+"
+sinkParallelism="128"
+sinkDatabase="ads"
+sinkTable="ads_user_dispatch_log_tag_df"
+doris \
+  -sparkSql "${sparkSql}" \
+  -sinkParallelism "${sinkParallelism}" \
+  -sinkDatabase "${sinkDatabase}" \
+  -sinkTable "${sinkTable}"
--- page4.sql
+++ page4.sql
@@ -1,25 +1,28 @@
-DROP TABLE `ads`.`ads_user_dispatch_log_tag_df`;
-CREATE TABLE `ads`.`ads_user_dispatch_log_tag_df` (
-  `tyc_user_id` largeint COMMENT '天眼查用户ID',
-  `dispatch_id` largeint COMMENT '分发ID',
-  `pt` datev2 COMMENT '分区',
-  `dispatch_date` datev2 COMMENT '分发日期',
-  `mobile` string COMMENT '手机号'
-) ENGINE=OLAP
-DUPLICATE KEY(`tyc_user_id`, `dispatch_id`, `pt`)
-COMMENT '分发策略标签明细表'
-PARTITION BY RANGE(`pt`) ()
-DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 30
-PROPERTIES (
-  -- 副本数
-  "replication_allocation" = "tag.location.default: 3",
-  -- partition
-  "dynamic_partition.enable" = "true",
-  "dynamic_partition.time_unit" = "DAY",
-  "dynamic_partition.create_history_partition" = "true",
-  "dynamic_partition.history_partition_num" = "1",
-  "dynamic_partition.start" = "-1",
-  "dynamic_partition.end" = "1",
-  "dynamic_partition.prefix" = "p"
-);
------------------------------------------------------------------------------------------------------
+select count(1) from ads.ads_user_tag;
+select count(1) from dwd.dwd_user_register_details;
+select count(1) from dim.dim_user_comparison;
+select count(1) from dwd.dwd_coupon_info;
+
+create table bak.dwd_user_register_details as
+select cast(tyc_user_id as string) tyc_user_id,cast(mobile as string) mobile,cast(register_time as string) register_time,cast(vip_from_time as string) vip_from_time,cast(vip_to_time as string) vip_to_time,cast(user_type as string) user_type,cast(create_time as string) create_time,cast(update_time as string) update_time
+from dwd.dwd_user_register_details;
+
+create table bak.dwd_coupon_info as
+select cast(promotion_code as string) promotion_codeselect cast(unique_user_id as string) unique_user_idselect cast(promotion_id as string) promotion_idselect cast(use_status as string) use_statusselect cast(receive_time as string) receive_timeselect cast(effective_time as string) effective_timeselect cast(expiration_time as string) expiration_timeselect cast(create_time as string) create_timeselect cast(update_time as string) update_time
+from dwd.dwd_coupon_info;
+
+create table bak.dwd_app_active as
+select cast(app_id2 as string) app_id2,cast(pt as string) pt,cast(android_id as string) android_id,cast(imei as string) imei,cast(oaid as string) oaid,cast(idfa as string) idfa,cast(idfv as string) idfv,cast(type as string) type,cast(umeng_channel as string) umeng_channel,cast(create_time as string) create_time,cast(app_version as string) app_version,cast(update_time as string) update_time
+from dwd.dwd_app_active;
+
+create table bak.dim_user_comparison as
+select cast(uid as string) uid,cast(tyc_user_id as string) tyc_user_id,cast(tyc_user_id_new as string) tyc_user_id_new,cast(mobile as string) mobile,cast(create_time as string) create_time,cast(update_time as string) update_time
+from dim.dim_user_comparison;
+
+create table bak.dwd_order_info as
+select cast(order_id as string) order_id,cast(order_code as string) order_code,cast(tyc_user_id as string) tyc_user_id,cast(mobile as string) mobile,cast(sku_id as string) sku_id,cast(vip_from_time as string) vip_from_time,cast(vip_to_time as string) vip_to_time,cast(order_status as string) order_status,cast(amount as string) amount,cast(actual_amount as string) actual_amount,cast(invite_code as string) invite_code,cast(pay_date as string) pay_date,cast(create_date as string) create_date,cast(platform_name as string) platform_name,cast(pay_way as string) pay_way,cast(pay_point_id as string) pay_point_id,cast(create_time as string) create_time,cast(update_time as string) update_time
+from dwd.dwd_order_info;
+
+
+
+
