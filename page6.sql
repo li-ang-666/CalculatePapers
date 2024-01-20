0eac3550542464c591fbb6e35e9108bc19bc204e
liang <li_ang_666@163.com>
2024-01-20 12:36:07 +0800

    1

---
+++ page6.sql
@@ -0,0 +1,11 @@
+select count(1) from ads.ads_user_dispatch_log_tag_df;
+select count(1) from ads.ads_user_tag;
+select count(1) from dwd.dwd_user_register_details;
+select count(1) from dim.dim_user_comparison;
+select count(1) from dwd.dwd_coupon_info;
+select count(1) from ads.ads_user_distribution_user_tag_df;
+select count(1) from ads.ads_user_tag_commercial_df;
+select count(1) from crowd.crowd_user_bitmap;
+select count(1) from crowd.dispatch_user_bitmap;
+select count(1) from crowd.dispatch_sub_user_bitmap;
+
