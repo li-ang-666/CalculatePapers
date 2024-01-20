5d697742404871a5de49c64ac9adf18fa3ab5b1f
liang <li_ang_666@163.com>
2024-01-20 11:01:04 +0800

    1

--- page5.sql
+++ page5.sql
@@ -1,13 +1,39 @@
-./flink-run.sh dwd_app_active.yml
-./flink-run.sh dwd_basic_data_collect_monitor_hours.yml
-./flink-run.sh dwd_coupon_info.yml
-./flink-run.sh dwd_dispatch_task.yml
-./flink-run.sh dwd_pay_point_com_detail.yml
-./flink-run.sh dwd_user_register_details.yml
+CREATE TABLE `crowd_user_bitmap` (
+  `crowd_id` largeint COMMENT '人群包id',
+  `create_timestamp` largeint COMMENT '创建时间',
+  `user_id_bitmap` bitmap BITMAP_UNION COMMENT '用户id位图'
+)
+AGGREGATE KEY(`crowd_id`, `create_timestamp`)
+DISTRIBUTED BY HASH(`crowd_id`) BUCKETS 30
+PROPERTIES (
+  -- 副本数
+  "replication_allocation" = "tag.location.default: 3"
+);
 
-dwd.dwd_app_active
-dwd.dwd_basic_data_collect_monitor_hours
-dwd.dwd_coupon_info
-dwd.dwd_dispatch_task
-dwd.dwd_pay_point_com_detail
-dwd.dwd_user_register_details
+CREATE TABLE `dispatch_user_bitmap` (
+  `dispatch_id` largeint COMMENT '分发策略id',
+  `create_timestamp` largeint COMMENT '创建时间',
+  `user_id_count` largeint COMMENT '用户id数量',
+  `user_id_bitmap` bitmap BITMAP_UNION COMMENT '用户id位图'
+)
+AGGREGATE KEY(`dispatch_id`, `create_timestamp`, `user_id_count`)
+DISTRIBUTED BY HASH(`dispatch_id`) BUCKETS 30
+PROPERTIES (
+  -- 副本数
+  "replication_allocation" = "tag.location.default: 3"
+);
+
+CREATE TABLE `dispatch_sub_user_bitmap` (
+  `dispatch_id` largeint COMMENT '分发策略id',
+  `create_timestamp` largeint COMMENT '创建时间',
+  `user_id_count` largeint COMMENT '用户id数量',
+  `sub_num` largeint COMMENT '子编号',
+  `result_type` largeint COMMENT '结果',
+  `user_id_bitmap` bitmap BITMAP_UNION COMMENT '用户id位图'
+)
+AGGREGATE KEY(`dispatch_id`, `create_timestamp`, `user_id_count`, `sub_num`, `result_type`)
+DISTRIBUTED BY HASH(`dispatch_id`) BUCKETS 30
+PROPERTIES (
+  -- 副本数
+  "replication_allocation" = "tag.location.default: 3"
+);
