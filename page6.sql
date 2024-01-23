38cda89e951a95944948a477384d0d3df1f6097e
liang <li_ang_666@163.com>
2024-01-23 10:23:48 +0800

    1

--- page6.sql
+++ page6.sql
@@ -12,7 +12,7 @@ select count(1) from crowd.dispatch_sub_user_bitmap;
 -- hive
 set spark.executor.memory=8g;
 with t as(
-select '-1' crowd_id, '-1' create_timestamp, doris.to_bitmap(id) user_id_bitmap from (select id from ods.ods_company_base_company_index_df where pt = '20240121' limit 12345678)tb
+select '-1' crowd_id, '-1' create_timestamp, doris.to_bitmap(id) user_id_bitmap from hudi_ods.company_index
 )insert overwrite table project.crowd_user_bitmap partition(pt = '-1') select * from t;
 
 select pt, crowd_id, create_timestamp, doris.bitmap_count(user_id_bitmap) cnt from project.crowd_user_bitmap where pt = '-1';
@@ -25,4 +25,10 @@ select * from crowd.crowd_user_bitmap_tasks;
 
 select bitmap_count(user_id_bitmap) from crowd.crowd_user_bitmap where crowd_id = -1;
 
+
+with t0 as(
+    select * from hudi_ods.company_index limit 50000000
+),t as(
+    select '-1' crowd_id, '-1' create_timestamp, doris.to_bitmap(id) user_id_bitmap from t0
+)insert overwrite table project.crowd_user_bitmap partition(pt = '-1') select * from t;
 
