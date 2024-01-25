f821abaa257e6adb447cecc7c8ff017a0a0868ae
liang <li_ang_666@163.com>
2024-01-25 22:03:58 +0800

    1

--- page8.sql
+++ page8.sql
@@ -44,5 +44,27 @@ spark-sql --master yarn spark-sql --master yarn --conf spark.driver.host=10.99.1
   --conf spark.sql.autoBroadcastJoinThreshold=104857600
 
 
+create table if not exists test.graph_export_node_0(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00000*';
+create table if not exists test.graph_export_node_1(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00001*';
+create table if not exists test.graph_export_node_2(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00002*';
+create table if not exists test.graph_export_node_3(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00003*';
+create table if not exists test.graph_export_node_4(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00004*';
+create table if not exists test.graph_export_node_5(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00005*';
+create table if not exists test.graph_export_node_6(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00006*';
+create table if not exists test.graph_export_node_7(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00007*';
+create table if not exists test.graph_export_node_8(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00008*';
+create table if not exists test.graph_export_node_9(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00009*';
+create table if not exists test.graph_export_node_10(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00010*';
+create table if not exists test.graph_export_node_11(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00011*';
+create table if not exists test.graph_export_node_12(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00012*';
+create table if not exists test.graph_export_node_13(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00013*';
+create table if not exists test.graph_export_node_14(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00014*';
+create table if not exists test.graph_export_node_15(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00015*';
+create table if not exists test.graph_export_node_16(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00016*';
+create table if not exists test.graph_export_node_17(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00017*';
+create table if not exists test.graph_export_node_18(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00018*';
+create table if not exists test.graph_export_node_19(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00019*';
+create table if not exists test.graph_export_node_20(`row` string)stored as textfile location 'obs://hadoop-obs/flink/graph/node/part-00020*';
+
 
 
