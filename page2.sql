dc22602d467be4d72ef8667ce22be90e18f1338f
liang <li_ang_666@163.com>
2024-01-25 22:09:25 +0800

    1

--- page2.sql
+++ page2.sql
@@ -8,7 +8,7 @@ CREATE TABLE `ads`.`ads_user_dispatch_log_tag_df` (
 )
 DUPLICATE KEY(`tyc_user_id`, `dispatch_id`, `pt`)
 PARTITION BY RANGE(`pt`) ()
-DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 30
+DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 40
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3",
@@ -20,7 +20,7 @@ PROPERTIES (
   "dynamic_partition.start" = "-1",
   "dynamic_partition.end" = "1",
   "dynamic_partition.prefix" = "p",
-  "dynamic_partition.buckets" = "30"
+  "dynamic_partition.buckets" = "40"
 );
 
 -----------------------------------------------------------------------------------------------------
@@ -88,7 +88,7 @@ CREATE TABLE `ads`.`ads_user_distribution_user_tag_df` (
 )
 UNIQUE KEY(`tyc_user_id`, `pt`)
 PARTITION BY RANGE(`pt`) ()
-DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 30
+DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 40
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3",
@@ -100,7 +100,7 @@ PROPERTIES (
   "dynamic_partition.start" = "-1",
   "dynamic_partition.end" = "1",
   "dynamic_partition.prefix" = "p",
-  "dynamic_partition.buckets" = "30"
+  "dynamic_partition.buckets" = "40"
 );
 
 -----------------------------------------------------------------------------------------------------
@@ -148,7 +148,7 @@ CREATE TABLE `ads`.`ads_user_tag_commercial_df` (
 )
 UNIQUE KEY(`tyc_user_id`, `pt`)
 PARTITION BY RANGE(`pt`) ()
-DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 30
+DISTRIBUTED BY HASH(`tyc_user_id`) BUCKETS 40
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3",
@@ -160,5 +160,5 @@ PROPERTIES (
   "dynamic_partition.start" = "-1",
   "dynamic_partition.end" = "1",
   "dynamic_partition.prefix" = "p",
-  "dynamic_partition.buckets" = "30"
+  "dynamic_partition.buckets" = "40"
 );
--- page5.sql
+++ page5.sql
@@ -4,7 +4,7 @@ CREATE TABLE `crowd_user_bitmap` (
   `user_id_bitmap` bitmap BITMAP_UNION COMMENT '用户id位图'
 )
 AGGREGATE KEY(`crowd_id`, `create_timestamp`)
-DISTRIBUTED BY HASH(`crowd_id`) BUCKETS 30
+DISTRIBUTED BY HASH(`crowd_id`) BUCKETS 80
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
@@ -17,7 +17,7 @@ CREATE TABLE `dispatch_user_bitmap` (
   `user_id_bitmap` bitmap BITMAP_UNION COMMENT '用户id位图'
 )
 AGGREGATE KEY(`dispatch_id`, `create_timestamp`, `user_id_count`)
-DISTRIBUTED BY HASH(`dispatch_id`) BUCKETS 30
+DISTRIBUTED BY HASH(`dispatch_id`) BUCKETS 80
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
@@ -32,7 +32,7 @@ CREATE TABLE `dispatch_sub_user_bitmap` (
   `user_id_bitmap` bitmap BITMAP_UNION COMMENT '用户id位图'
 )
 AGGREGATE KEY(`dispatch_id`, `create_timestamp`, `user_id_count`, `sub_num`, `result_type`)
-DISTRIBUTED BY HASH(`dispatch_id`) BUCKETS 30
+DISTRIBUTED BY HASH(`dispatch_id`) BUCKETS 80
 PROPERTIES (
   -- 副本数
   "replication_allocation" = "tag.location.default: 3"
