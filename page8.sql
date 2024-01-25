01bf126e6316d9d6486970f9db0fdef9a5998eee
liang <li_ang_666@163.com>
2024-01-25 09:51:53 +0800

    1

--- page8.sql
+++ page8.sql
@@ -26,13 +26,23 @@ select count(1) from dwd.dwd_dispatch_task;
 select count(1) from dwd.dwd_basic_data_collect_monitor_hours;
 select count(1) from ads.ads_user_tag;
 
+spark-beeline --master yarn --driver-memory 2g
 
-spark-beeline \
---hiveconf spark.driver.memory=2g \
---hiveconf spark.driver.memoryOverhead=512m \
---hiveconf spark.executor.memory=8g \
---hiveconf spark.executor.memoryOverhead=512m \
---hiveconf spark.executor.cores=4 \
---hiveconf spark.memory.fraction=0.8 \
---hiveconf spark.yarn.priority=999 \
---hiveconf spark.dynamicAllocation.maxExecutors=32
+
+spark-sql --master yarn spark-sql --master yarn --conf spark.driver.host=10.99.194.128 \
+  --driver-memory 2g --driver-cores 1 \
+  --conf spark.driver.memoryOverhead=512m \
+  --executor-memory 8g --num-executors 64 --executor-cores 2 \
+  --conf spark.executor.memoryOverhead=512m \
+  --queue offline \
+  --conf spark.memory.fraction=0.8 \
+  --conf spark.driver.extraJavaOptions=-Dlog4j.configuration=file:////home/hive/liang/doris/log4j-all.properties
+  --conf spark.yarn.maxAppAttempts=1 \
+  --conf spark.yarn.priority=999 \
+  --conf spark.sql.shuffle.partitions=1024 \
+  --conf spark.shuffle.io.maxRetries=10 \
+  --conf spark.sql.autoBroadcastJoinThreshold=104857600
+
+
+
+
