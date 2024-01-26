5fecc2b07ff1515f3f6c730520932d7cfe6b66b9
liang <li_ang_666@163.com>
2024-01-26 22:35:23 +0800

    1

--- page10.sql
+++ page10.sql
@@ -1,18 +1,38 @@
 ,34,35,36,37,38,39,40,43,44,46,47,48,49,51,52,53,54,55,56,57,58,59,60,61,67,68
 
 
-
-
-create table if not exists test.graph_export_node_invalid(
-  `row` string
-)
-row format delimited
+drop table test.graph_export_node_invalid_new;
+create table if not exists test.graph_export_node_invalid_new(
+  `row1` string,
+  `row2` string,
+  `row3` string,
+  `row4` string,
+  `row5` string,
+  `row6` string,
+  `row7` string,
+  `row8` string,
+  `row9` string
+)row format delimited
 fields terminated by ','
 lines terminated by '\n'
-stored as textfile location 'obs://hadoop-obs/flink/graph/node_invalid';
+stored as textfile location 'obs://hadoop-obs/flink/graph/node_invalid_new';
 
 
-
+drop table test.graph_export_node_invalid_diff;
+create table if not exists test.graph_export_node_invalid_diff(
+  `row1` string,
+  `row2` string,
+  `row3` string,
+  `row4` string,
+  `row5` string,
+  `row6` string,
+  `row7` string,
+  `row8` string,
+  `row9` string
+)row format delimited
+fields terminated by ','
+lines terminated by '\n'
+stored as textfile location 'obs://hadoop-obs/flink/graph/node_invalid_diff';
 
 
 
@@ -22,10 +42,10 @@ with t1 as (
  group by company_id
 ),
 t2 as (
- select company_id,max(company_name) from ods.ods_company_base_company_index_df
+ select company_id,max(company_name) company_name from ods.ods_company_base_company_index_df
  where pt = '20240125' and dw_is_del = 0
  group by company_id
-)insert overwrite table test.graph_export_node_invalid
+)insert overwrite table test.graph_export_node_invalid_new
 select /*+ REPARTITION(4) */
  t1.company_id row1,
  'node' row2,
@@ -37,3 +57,10 @@ select /*+ REPARTITION(4) */
  'false' row8,
  '1704038400000' row9
 from t1 join t2 on t1.company_id = t2.company_id;
+
+
+with t as(
+    select t1.* from test.graph_export_node_invalid_new t1 left anti join test.graph_export_node_invalid t2 on t1.row1 = t2.row1
+)
+insert overwrite table test.graph_export_node_invalid_diff
+select /*+ REPARTITION(1) */ * from t;
