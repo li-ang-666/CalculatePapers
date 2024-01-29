e715af6f134d23fb9aaf98d6eb8d42fd390b4884
liang <li_ang_666@163.com>
2024-01-29 17:12:19 +0800

    1

--- page8.sql
+++ page8.sql
@@ -7,12 +7,21 @@
 
 
 set spark.driver.memory=1g;
-set spark.driver.memoryOverhead=256m;
+set spark.driver.memoryOverhead=512m;
 set spark.executor.num=1;
 set spark.executor.cores=1;
 set spark.executor.memory=4g;
-set spark.executor.memoryOverhead=256m;
+set spark.executor.memoryOverhead=512m;
 set spark.memory.fraction=0.01;
 
+
+
+INSERT INTO test.crowd_user_bitmap PARTITION(pt=20240128) 
+SELECT 1, -1, doris.bitmap_union(t.uid) 
+FROM (SELECT 1 c, doris.to_bitmap(t1.old_user_id) uid FROM dim_offline.dim_user_comparison_df t1 where t1.pt=20240128 and t1.old_user_id regexp '^\\d+$') t GROUP BY t.c
+
+
+
+SELECT count(1) FROM dim_offline.dim_user_comparison_df t1 where t1.pt=20240128 and t1.old_user_id regexp '^\\d+$'
 
 
