d9e8bd9c2cc29ca6489388e8364409db3966080f
liang <li_ang_666@163.com>
2024-02-27 19:42:00 +0800

    1

--- page4.sql
+++ page4.sql
@@ -25,3 +25,23 @@ CREATE TABLE `prism_shareholder_path`.`ratio_path_company_new` (
   KEY `idx_company` (`company_id`),
   KEY `idx_shareholder` (`shareholder_id`,`company_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='投资路径结果表';
+
+
+drop table `bdp_equity`.`entity_controller_details_new`;
+CREATE TABLE `bdp_equity`.`entity_controller_details_new` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `tyc_unique_entity_id` varchar(50) NOT NULL DEFAULT '' COMMENT '实体ID',
+  `entity_type_id` tinyint(4) NOT NULL DEFAULT '0' COMMENT '实体类型',
+  `entity_name_valid` varchar(255) NOT NULL DEFAULT '' COMMENT '实体名称',
+  `company_name_controlled` varchar(255) NOT NULL DEFAULT '' COMMENT '被控制的企业名称',
+  `company_id_controlled` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '被控制的企业ID',
+  `equity_relation_path_cnt` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '关联路径数',
+  `estimated_equity_ratio_total` decimal(26,6) NOT NULL DEFAULT '0' COMMENT '预计总持股比例',
+  `controlling_equity_relation_path_detail` longtext COMMENT '实际控制关系路径详情',
+  `is_controller_tyc_unique_entity_id` tinyint(4) NOT NULL DEFAULT '1' COMMENT '实体是否为实际控制人',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_company` (`company_id_controlled`),
+  KEY `idx_shareholder` (`tyc_unique_entity_id`,`company_id_controlled`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='实际控制人/对外控制企业';
