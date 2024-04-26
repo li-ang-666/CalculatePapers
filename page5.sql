ebf89fca761f8ff1faf53a5c3127f210bfdade03
liang <li_ang_666@163.com>
2024-04-26 19:32:33 +0800

    1

--- page5.sql
+++ page5.sql
@@ -50,15 +50,30 @@ with equity_ratio as(
 ),
 company_equity_relation_details as(
   select * from ods.ods_graph_data_company_equity_relation_details_df where reference_pt_year = 2024 and pt = 20240425 and dw_is_del = 0
-)
-select distinct if(t1.id is not null,t1.company_graph_id,t2.company_id_invested) id
+),
+diff as (
+select distinct if(t1.id is not null,t1.company_graph_id,t2.company_id_invested) company_id
 from equity_ratio t1
 full outer join company_equity_relation_details t2
 on t1.company_graph_id = t2.company_id_invested and t1.shareholder_graph_id = t2.company_id_investor
 where t2.id is null or t1.id is null
-order by id
-;
+),
+bigger_than_1 as (
+  select distinct company_id from ods.ods_prism_shareholder_path_ratio_path_company_new_all_df where investment_ratio_total > 1 and pt = 20240425 and dw_is_del = 0
+)
+select distinct company_id from
+(
+select company_id from diff
+union all
+select company_id from bigger_than_1
+) t
+order by company_id;
+
+
 
 
 select distinct company_id from ods.ods_prism_shareholder_path_ratio_path_company_new_all_df where investment_ratio_total > 1 and pt = 20240425 and dw_is_del = 0
+
+
+
 
