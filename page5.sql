3df6f20b560c8ed1a6cc482f45ff5859115f73c2
liang <li_ang_666@163.com>
2024-05-15 17:12:03 +0800

    1

--- page5.sql
+++ page5.sql
@@ -1,60 +1,17 @@
-{ "000" :"[checkpoint] table: equity_ratio, pivot: 7,785,473, upperBound: 625,568,150,424,790, lag: 625,568,142,639,317",
-"001" :"[checkpoint] table: equity_ratio, pivot: 1,251,136,300,849,579, upperBound: 1,251,136,300,849,579, lag: 0",
-"002" :"[checkpoint] table: equity_ratio, pivot: 1,876,704,451,274,368, upperBound: 1,876,704,451,274,368, lag: 0",
-"003" :"[checkpoint] table: equity_ratio, pivot: 2,502,272,601,699,157, upperBound: 2,502,272,601,699,157, lag: 0",
-"004" :"[checkpoint] table: equity_ratio, pivot: 3,127,840,752,123,946, upperBound: 3,127,840,752,123,946, lag: 0",
-"005" :"[checkpoint] table: equity_ratio, pivot: 3,753,408,902,548,735, upperBound: 3,753,408,902,548,735, lag: 0",
-"006" :"[checkpoint] table: equity_ratio, pivot: 4,378,977,052,973,524, upperBound: 4,378,977,052,973,524, lag: 0",
-"007" :"[checkpoint] table: equity_ratio, pivot: 5,004,545,203,398,313, upperBound: 5,004,545,203,398,313, lag: 0",
-"008" :"[checkpoint] table: equity_ratio, pivot: 5,630,113,353,823,102, upperBound: 5,630,113,353,823,102, lag: 0",
-"009" :"[checkpoint] table: equity_ratio, pivot: 6,255,681,504,247,891, upperBound: 6,255,681,504,247,891, lag: 0",
-"010" :"[checkpoint] table: equity_ratio, pivot: 6,881,249,654,672,680, upperBound: 6,881,249,654,672,680, lag: 0",
-"011" :"[checkpoint] table: equity_ratio, pivot: 7,506,817,805,097,469, upperBound: 7,506,817,805,097,469, lag: 0",
-"012" :"[checkpoint] table: equity_ratio, pivot: 8,132,385,955,522,258, upperBound: 8,132,385,955,522,258, lag: 0",
-"013" :"[checkpoint] table: equity_ratio, pivot: 8,757,954,105,947,047, upperBound: 8,757,954,105,947,047, lag: 0",
-"014" :"[checkpoint] table: equity_ratio, pivot: 9,383,522,256,371,836, upperBound: 9,383,522,256,371,836, lag: 0",
-"015" :"[checkpoint] table: equity_ratio, pivot: 10,009,090,356,663,400, upperBound: 10,009,090,406,796,625, lag: 50,133,225" }
+drop table if exists test.lt;
+create external table if not exists test.lt (
+id                                          string,
+tyc_unique_entity_id                        string,
+entity_type_id                              string,
+entity_name_valid                           string,
+control_graph_data_application_type         string,
+total_entity_cnt_through_multi_control_path string,
+min_graph_tier_through_multi_control_path   string,
+max_graph_tier_through_multi_control_path   string,
+create_time                                 string,
+update_time                                 string
+)
+ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
+STORED AS TEXTFILE location 'obs://hadoop-obs/flink/test/';
 
-
-
-alter table hudi_ads.cooperation_partner drop partition(pt=0);
-alter table hudi_ads.cooperation_partner_diff drop partition(pt=0);
-alter table hudi_ads.cooperation_partner drop partition(pt=1);
-alter table hudi_ads.cooperation_partner_diff drop partition(pt=1);
-
-
-truncate table hudi_ads.cooperation_partner;
-truncate table hudi_ads.cooperation_partner_diff;
-
-
-alter table hudi_ads.cooperation_partner partition(pt=1) rename to partition(pt=0);
-
-
-
-select * from hudi_ads.cooperation_partner where pt=0
-and boss_human_pid = '001U9MR09RKBKBS09'
-and partner_human_pid = '00139MR09R5B07SMU'
-and company_gid = 2344040450
-;
-
-
-SELECT t.*
-          FROM hudi_ads.cooperation_partner t
-          WHERE pt=20240513 and boss_identity like '%股东(持股%' and boss_shares is null
-          LIMIT 100
-          ;
-
-
-SELECT count(1)
-          FROM hudi_ads.cooperation_partner t
-          WHERE pt=0 and boss_identity like '%股东(持股%' and boss_shares is null
-          LIMIT 100
-          ;
-
-
-select boss_human_pid,boss_human_pid from hudi_ads.cooperation_partner_diff where pt=20240510
-and boss_human_pid = 'Q0Q9YMV00KENQH40U'
-and partner_human_pid = '90FDRML09G1L2NTN2'
-and company_gid = 289862943;
-                                             
 
