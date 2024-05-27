88ddc7e5915ef1af0de7f3f73b6a9ce9f7b3f0e2
liang <li_ang_666@163.com>
2024-05-27 16:53:59 +0800

    1

--- page5.sql
+++ page5.sql
@@ -30,6 +30,38 @@ create external table test.lt
 STORED AS TEXTFILE
 LOCATION 'obs://hadoop-obs/flink/test';
 
+drop table test.lt2;
+create external table test.lt2
+(
+    id                                  string,
+    boss_human_gid                      string,
+    boss_human_pid                      string,
+    boss_human_name                     string,
+    boss_identity                       string,
+    boss_shares                         string,
+    company_gid                         string,
+    company_name                        string,
+    company_registered_status           string,
+    company_registered_capital          string,
+    s                                   string,
+    partner_human_gid                   string,
+    partner_human_pid                   string,
+    partner_human_name                  string,
+    partner_identity                    string,
+    partner_shares                      string,
+    single_cooperation_score            string,
+    multi_cooperation_score             string,
+    single_cooperation_row_number       string,
+    multi_cooperation_dense_rank        string,
+    cooperation_times_with_this_partner string,
+    cooperation_times_with_all_partner  string,
+    total_partners                      string,
+    create_time                         string,
+    update_time                         string
+)
+STORED AS parquet
+LOCATION 'obs://hadoop-obs/flink/pqt';
+
 
 
 drop table test.group_empty;
