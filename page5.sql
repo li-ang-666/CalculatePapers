c49b500c1129f9e3a67d7e09e610c1cb8a09e209
liang <li_ang_666@163.com>
2024-06-06 16:49:19 +0800

    1

--- page5.sql
+++ page5.sql
@@ -1,90 +1,27 @@
-drop table test.lt;
-create external table test.lt
-(
-    id                                  string,
-    boss_human_gid                      string,
-    boss_human_pid                      string,
-    boss_human_name                     string,
-    boss_identity                       string,
-    boss_shares                         string,
-    company_gid                         string,
-    company_name                        string,
-    company_registered_status           string,
-    company_registered_capital          string,
-    s                                   string,
-    partner_human_gid                   string,
-    partner_human_pid                   string,
-    partner_human_name                  string,
-    partner_identity                    string,
-    partner_shares                      string,
-    single_cooperation_score            string,
-    multi_cooperation_score             string,
-    single_cooperation_row_number       string,
-    multi_cooperation_dense_rank        string,
-    cooperation_times_with_this_partner string,
-    cooperation_times_with_all_partner  string,
-    total_partners                      string,
-    create_time                         string,
-    update_time                         string
-)ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
-STORED AS TEXTFILE
-LOCATION 'obs://hadoop-obs/flink/test';
-
-drop table test.lt2;
-create external table test.lt2
-(
-    id                                  string,
-    boss_human_gid                      string,
-    boss_human_pid                      string,
-    boss_human_name                     string,
-    boss_identity                       string,
-    boss_shares                         string,
-    company_gid                         string,
-    company_name                        string,
-    company_registered_status           string,
-    company_registered_capital          string,
-    s                                   string,
-    partner_human_gid                   string,
-    partner_human_pid                   string,
-    partner_human_name                  string,
-    partner_identity                    string,
-    partner_shares                      string,
-    single_cooperation_score            string,
-    multi_cooperation_score             string,
-    single_cooperation_row_number       string,
-    multi_cooperation_dense_rank        string,
-    cooperation_times_with_this_partner string,
-    cooperation_times_with_all_partner  string,
-    total_partners                      string,
-    create_time                         string,
-    update_time                         string
-)
-STORED AS parquet
-LOCATION 'obs://hadoop-obs/flink/pqt'
-tblproperties('parquet.compression'='GZIP');
+drop table company_patent_basic_info_index_split;
+CREATE TABLE `company_patent_basic_info_index_split` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `company_patent_basic_info_index_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'company_patent_basic_info_index表的主键id',
+  `company_patent_basic_info_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'company_patent_basic_info表的主键id',
+  `company_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '公司id',
+  `patent_type` int NOT NULL DEFAULT '0' COMMENT '专利类型',
+  `patent_publish_year` int NOT NULL DEFAULT '0' COMMENT '发布年份 ',
+  `patent_application_year` int NOT NULL DEFAULT '0' COMMENT '申请年份',
+  `patent_title` varchar(1000) NOT NULL DEFAULT '' COMMENT '专利名称',
+  `patent_application_number` varchar(50) NOT NULL DEFAULT '' COMMENT '申请号',
+  `patent_announce_number` varchar(50) NOT NULL DEFAULT '' COMMENT '公开号',
+  `patent_status` varchar(255) NOT NULL DEFAULT '' COMMENT '专利状态',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_company_patent_basic_info_index_id` (`company_patent_basic_info_index_id`) COMMENT 'company_patent_basic_info_index_id',
+  KEY `idx_company_patent_basic_info_id` (`company_patent_basic_info_id`) COMMENT 'company_patent_basic_info_id',
+  KEY `idx_patent_application_number` (`patent_application_number`) COMMENT 'patent_application_number',
+  KEY `idx_patent_announce_number` (`patent_announce_number`) COMMENT 'patent_announce_number',
+  KEY `idx_company_id_patent_type_pub_year` (`company_id`,`patent_type`,`patent_publish_year`) COMMENT 'company_id,patent_type,patent_publish_year',
+  KEY `idx_company_id_patent_type_app_year` (`company_id`,`patent_type`,`patent_application_year`) COMMENT 'company_id,patent_type,patent_application_year',
+  KEY `idx_company_id_patent_status` (`company_id`,`patent_status`) COMMENT 'company_id,patent_status'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='专利索引拆分表';
 
 
-drop table test.lt3;
-create external table test.lt3
-(
-    mobile                                  string
-)
-LOCATION 'obs://hadoop-obs/hive/warehouse/dmp.db/dmp_user_portrait_enterprise_label_di/pt=20230429';
-
-
-
-
-drop table test.group_empty;
-create external table test.group_empty
-(
-company_id string,
-company_name string
-)ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
-STORED AS TEXTFILE
-LOCATION 'obs://hadoop-obs/flink/group';
-
-
-select t1.*,t2.* from test.group_empty t1 left join test.tyc_group t2 on t1.company_id = t2.company_id order by t1.company_name;
-
-select t1.*,t2.* from test.group_empty t1 left join test.tyc_group t2 on t1.company_id = t2.group_id order by t1.company_name;
 
