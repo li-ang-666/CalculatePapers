0f6e8876f6d65db3784f3c04c2e25abca49e2059
liang <li_ang_666@163.com>
2024-06-07 17:37:02 +0800

    1

--- page5.sql
+++ page5.sql
@@ -4,13 +4,14 @@ CREATE TABLE `company_patent_basic_info_index_split` (
   `company_patent_basic_info_index_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'company_patent_basic_info_index表的主键id',
   `company_patent_basic_info_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'company_patent_basic_info表的主键id',
   `company_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '公司id',
-  `patent_type` int(11) NOT NULL DEFAULT '0' COMMENT '专利类型',
+  `patent_type` int(11) NOT NULL DEFAULT '0' COMMENT '专利类型: 1-发明类型 2-实用新型 3-外观设计',
   `patent_publish_year` int(11) NOT NULL DEFAULT '0' COMMENT '发布年份 ',
   `patent_application_year` int(11) NOT NULL DEFAULT '0' COMMENT '申请年份',
+  `patent_status` int(11) NOT NULL DEFAULT '0' COMMENT '专利状态 10-有效 20-失效 30-审中 40-无效',
+  `patent_status_detail` varchar(255) NOT NULL DEFAULT '' COMMENT '专利状态二级',
   `patent_title` varchar(1000) NOT NULL DEFAULT '' COMMENT '专利名称',
   `patent_application_number` varchar(50) NOT NULL DEFAULT '' COMMENT '申请号',
   `patent_announce_number` varchar(50) NOT NULL DEFAULT '' COMMENT '公开号',
-  `patent_status` varchar(255) NOT NULL DEFAULT '' COMMENT '专利状态',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
@@ -20,7 +21,7 @@ CREATE TABLE `company_patent_basic_info_index_split` (
   KEY `idx_patent_announce_number` (`patent_announce_number`) COMMENT 'patent_announce_number',
   KEY `idx_company_id_patent_type_pub_year` (`company_id`,`patent_type`,`patent_publish_year`) COMMENT 'company_id,patent_type,patent_publish_year',
   KEY `idx_company_id_patent_type_app_year` (`company_id`,`patent_type`,`patent_application_year`) COMMENT 'company_id,patent_type,patent_application_year',
-  KEY `idx_company_id_patent_status` (`company_id`,`patent_status`) COMMENT 'company_id,patent_status'
+  KEY `idx_company_id_patent_status_detail` (`company_id`,`patent_status_detail`) COMMENT 'company_id,patent_status_detail'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='专利索引拆分表';
 
 
