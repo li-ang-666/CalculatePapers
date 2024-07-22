867a5f47b4edb5b9e99bde585442ce2d6fe4e1b3
liang <li_ang_666@163.com>
2024-07-22 11:03:40 +0800

    1

--- page6.sql
+++ page6.sql
@@ -21,31 +21,4 @@ create table bid_ai_interface_v2(
   UNIQUE KEY `uniq_uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='招投标算法接口V2';
 
-CREATE TABLE `test` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) DEFAULT NULL,
-  `dt` date DEFAULT NULL,
-  `dtm` datetime DEFAULT NULL,
-  `tmsp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
-# For Spark versions: 3.0 - 3.1
-export SPARK_VERSION=3.1 # or 3.0
-spark-shell --packages org.apache.hudi:hudi-spark$SPARK_VERSION-bundle_2.12:0.15.0 \
---conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer' \
---conf 'spark.sql.extensions=org.apache.spark.sql.hudi.HoodieSparkSessionExtension' \
---conf 'spark.kryo.registrator=org.apache.spark.HoodieSparkKryoRegistrar'
-
-hudi_compaction_company_bond_plates
-hudi_compaction_company_clean_info
-hudi_compaction_company_equity_relation_details
-hudi_compaction_company_human_relation
-hudi_compaction_company_index
-hudi_compaction_company_legal_person
-hudi_compaction_entity_controller_details_new
-hudi_compaction_personnel
-hudi_compaction_senior_executive
-hudi_compaction_senior_executive_hk
-
 
