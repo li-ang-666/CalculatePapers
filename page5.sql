fbeb20df583b963dfc894576e72a759d2e7f96f6
liang <li_ang_666@163.com>
2024-07-24 15:58:34 +0800

    1

--- page5.sql
+++ page5.sql
@@ -1,3 +1,5 @@
+
+
 drop table company_patent_basic_info_index_split;
 CREATE TABLE `company_patent_basic_info_index_split` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
@@ -37,54 +39,4 @@ CREATE TABLE `company_patent_basic_info_index_split_statistic` (
   PRIMARY KEY (`id`),
   KEY `idx_company_id` (`company_id`) COMMENT 'company_id'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='专利统计表';
-
-
-drop table company_equity_relation_details_tmp;
-CREATE TABLE `company_equity_relation_details_tmp` (
-  -- new
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `company_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '公司gid',
-  `company_name` varchar(255) NOT NULL DEFAULT '' COMMENT '公司名称',
-  `shareholder_type` int(11) NOT NULL DEFAULT '0' COMMENT '股东类型，1代表人，2代表公司，3代表其他（非人非公司）',
-  `shareholder_type_show` int(11) NOT NULL DEFAULT '0' COMMENT '股东类型(包含公示类型)，1企业法人，2事业法人，3社团法人，4机关法人，5外商投资企业，6自然人股东，7外国(地区)投资者，8合伙企业，9个人独资企业，10其他投资者',
-  `shareholder_name_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '股东gid',
-  `shareholder_id` varchar(50) NOT NULL DEFAULT '' COMMENT '股东pid',
-  `shareholder_name` varchar(255) NOT NULL DEFAULT '' COMMENT '股东名称',
-  `equity_ratio` decimal(32,12) NOT NULL DEFAULT '0' COMMENT '持股比例',
-  `equity_amount` decimal(32,12) NOT NULL DEFAULT '0' COMMENT '认缴出资金额',
-  `equity_amount_currency` varchar(255) NOT NULL DEFAULT '' COMMENT '金额单位币种',
-  `share_type` varchar(255) NOT NULL DEFAULT '' COMMENT '股份类型',
-  `data_source` int(11) NOT NULL DEFAULT '0' COMMENT '出资比例计算的来源和规则（用于查错），-100：港股上市，100：数据来源于上市公司股东数据（包括十大股东和十大流通股东），非100:数据来源于工商或其他',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  -- old
-  `company_id_invested` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '被投资企业ID',
-  `tyc_unique_entity_id_invested` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '被投资方的天眼查唯一商查ID',
-  `tyc_unique_entity_name_invested` varchar(255) NOT NULL DEFAULT '' COMMENT '被投资方的天眼查唯一商查名称',
-  `investor_identity_type` int(11) NOT NULL DEFAULT '0' COMMENT '投资方身份属性，1代表人，2代表公司，3代表其他（非人非公司）',
-  `company_id_investor` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '投资企业ID',
-  `tyc_unique_entity_id_investor` varchar(255) NOT NULL DEFAULT '' COMMENT '投资方的天眼查唯一商查ID',
-  `tyc_unique_entity_name_investor` varchar(255) NOT NULL DEFAULT '' COMMENT '投资方的天眼查唯一商查名称',
-  `equity_relation_validation_year` int(11) NOT NULL DEFAULT '0' COMMENT '股权关系生效年份',
-  `reference_pt_year` int(11) NOT NULL DEFAULT '0' COMMENT '股权关系生效年份',
-  PRIMARY KEY (`id`),
-  KEY `idx_company_id` (`company_id`),
-  KEY `idx_shareholder_name_id` (`shareholder_name_id`),
-  KEY `idx_shareholder_id` (`shareholder_id`),
-  KEY `idx_invested` (`company_id_invested`,`reference_pt_year`,`company_id_investor`,`tyc_unique_entity_name_investor`),
-  KEY `idx_investor` (`tyc_unique_entity_id_investor`,`tyc_unique_entity_id_invested`,`reference_pt_year`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='股权明细';
-
-with equity_ratio as(
-    select * from ods_prism1_equity_ratio_df where pt = 20240703
-),hk as(
-    select * from ods_prism_stock_source_hk_stock_main_shareholder_df where pt = 20240703
-)
-select distinct company_id from(
-  select distinct company_graph_id company_id from equity_ratio where source = 100
-  union all
-  select distinct org_id company_id from hk
-)t;
-
-
 
