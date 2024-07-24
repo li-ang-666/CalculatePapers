652cab3e444c70ebc090e6efb987d89ee45f1b71
liang <li_ang_666@163.com>
2024-07-24 14:43:58 +0800

    1

--- page6.sql
+++ page6.sql
@@ -17,28 +17,23 @@ create table bid_ai_v1(
   `city` varchar(255) NOT NULL DEFAULT '' COMMENT '市',
   `v1` varchar(255) NOT NULL DEFAULT '' COMMENT '一级信息类型',
   `v2` varchar(255) NOT NULL DEFAULT '' COMMENT '二级信息类型',
-  `bid_type` varchar(255) NOT NULL DEFAULT '' COMMENT '招标方式',
   `bid_info` text COMMENT '算法解析原始json',
-  `contract_no` text COMMENT '合同编号',
-  `candidates` text COMMENT '候选机构',
-  `winners` text COMMENT '中标机构',
-  `winner_raw_amounts` text COMMENT '中标金额',
-  `winner_amounts` text COMMENT '中标金额',
-  `budget_raw_amounts` text COMMENT '预算金额',
-  `budget_amounts` text COMMENT '预算金额',
+  `purchaser` text COMMENT '招标单位、采购人',
+  `winner` text COMMENT '中标单位、供应商',
+  `winner_amount` text COMMENT '中标金额',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`),
   KEY `idx_uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='招投标算法接口V1';
 
-
+drop table bid_ai_v2;
 create table bid_ai_v2(
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `uuid` varchar(255) NOT NULL DEFAULT '' COMMENT '招投标uuid',
   `title` varchar(500) NOT NULL DEFAULT '' COMMENT '招投标title',
-  `content` text COMMENT '招投标content',
-  `bidding_unit` text COMMENT '招标单位',
+  `type` varchar(255) NOT NULL DEFAULT '' COMMENT '招投标type',
+  `bidding_unit` text COMMENT '投标单位',
   `tendering_proxy_agent` text COMMENT '代理单位',
   `bid_submission_deadline` text COMMENT '招标截止时间',
   `tender_document_acquisition_deadline` text COMMENT '标书下载截止时间',
@@ -48,5 +43,35 @@ create table bid_ai_v2(
   PRIMARY KEY (`id`),
   KEY `idx_uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='招投标算法接口V2';
+
+drop table company_bid_parsed_info_patch;
+create table company_bid_parsed_info_patch(
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `uuid` varchar(255) NOT NULL DEFAULT '' COMMENT 'uuid',
+  `title` varchar(500) NOT NULL DEFAULT '' COMMENT '标题',
+  `link` varchar(500) NOT NULL DEFAULT '' COMMENT '链接',
+  `publish_time` datetime NULL COMMENT '发布时间',
+  `type` varchar(255) NOT NULL DEFAULT '' COMMENT '类型',
+  `content_obs_index` varchar(255) NOT NULL DEFAULT '' COMMENT '类型',
+  `deleted` tinyint NOT NULL DEFAULT '0' COMMENT '是否 删除or屏蔽',
+  `province` varchar(255) NOT NULL DEFAULT '' COMMENT '省',
+  `city` varchar(255) NOT NULL DEFAULT '' COMMENT '市',
+  `v1` varchar(255) NOT NULL DEFAULT '' COMMENT '一级信息类型',
+  `v2` varchar(255) NOT NULL DEFAULT '' COMMENT '二级信息类型',
+  `bid_info` text COMMENT '算法解析原始json',
+  `purchaser` text COMMENT '招标单位、采购人',
+  `winner` text COMMENT '中标单位、供应商',
+  `winner_amount` text COMMENT '中标金额',
+  `bidding_unit` text COMMENT '投标单位',
+  `tendering_proxy_agent` text COMMENT '代理单位',
+  `bid_submission_deadline` text COMMENT '招标截止时间',
+  `tender_document_acquisition_deadline` text COMMENT '标书下载截止时间',
+  `project_number` text COMMENT '项目编号',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='招投标线上表';
+
 
 
