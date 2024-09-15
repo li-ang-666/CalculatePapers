2894dc03318cfcc890fb25d96f1b9873facf8458
liang <li_ang_666@163.com>
2024-09-15 12:34:38 +0800

    1

--- page4.sql
+++ page4.sql
@@ -1,23 +1,3 @@
-drop table test.company_bid_plus;
-CREATE TABLE `company_bid_plus` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `uuid` varchar(50) NOT NULL DEFAULT '' COMMENT 'UUID',
-  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '标题',
-  `link` varchar(255) NOT NULL DEFAULT '' COMMENT '详细信息链接',
-  `intro` varchar(255) NOT NULL DEFAULT '' COMMENT '正文简介',
-  `abs` longtext NOT NULL COMMENT '摘要信息',
-  `content` longtext NOT NULL COMMENT '正文信息',
-  `publish_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
-  `purchaser` varchar(255) NOT NULL DEFAULT '' COMMENT '采购人',
-  `proxy` varchar(255) NOT NULL DEFAULT '' COMMENT '代理机构',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除, 0 非删除, 1 删除, 2 hidden',
-  `province` varchar(255) NOT NULL DEFAULT '' COMMENT '省份',
-  `base` varchar(255) NOT NULL DEFAULT '',
-  `type` varchar(255) NOT NULL DEFAULT '',
-  `items` varchar(255) NOT NULL DEFAULT '',
-  `post_result` longtext NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_uuid` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='招投标信息';
+,46729694,47452367,47459274,47456975,47453092,47453766,47458474,47450648,47458731,46557760,47453900,47460110,47450328,47140163,47453088,47454510,47451132,47457404,45622678,47459891,47457149,47449980,47457973
+
+
